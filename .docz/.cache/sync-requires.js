2246ca25a9f2779c7a4cb0a04477f64c1a4d281a
Ethan Skinner <ethan.skinner@backlotcars.com>
2020-07-05 14:34:46 -0500

    add readme

--- .docz/.cache/sync-requires.js
+++ .docz/.cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---readme-md": hot(preferDefault(require("/Users/ethan/localdev/baseball-card/README.md"))),
   "component---src-index-mdx": hot(preferDefault(require("/Users/ethan/localdev/baseball-card/src/index.mdx"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/ethan/localdev/baseball-card/.docz/src/pages/404.js")))
 }
