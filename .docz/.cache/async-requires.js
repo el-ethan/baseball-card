de10655fde605f8770a873848496caba8b67d8bb
Ethan Skinner <ethan.skinner@backlotcars.com>
2020-07-06 21:15:25 -0500

    add more docz stuff

--- .docz/.cache/async-requires.js
+++ .docz/.cache/async-requires.js
@@ -2,6 +2,7 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
+  "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---readme-md": () => import("./../../README.md" /* webpackChunkName: "component---readme-md" */),
   "component---src-index-mdx": () => import("./../../src/index.mdx" /* webpackChunkName: "component---src-index-mdx" */),
   "component---src-pages-404-js": () => import("./../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */)
--- .docz/.cache/sync-requires.js
+++ .docz/.cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/ethan/localdev/baseball-card/.docz/.cache/dev-404-page.js"))),
   "component---readme-md": hot(preferDefault(require("/Users/ethan/localdev/baseball-card/README.md"))),
   "component---src-index-mdx": hot(preferDefault(require("/Users/ethan/localdev/baseball-card/src/index.mdx"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/ethan/localdev/baseball-card/.docz/src/pages/404.js")))
